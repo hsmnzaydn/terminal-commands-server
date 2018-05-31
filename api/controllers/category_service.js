6652036d02956cd0a16e842269b5a58626e7897a
hsmnzaydn <serkan.zaydn@gmail.com>
2018-05-31 17:58:39 +0300

    Implemented addCategory service Added new global variable ( TR,ENG,FRA )

--- api/controllers/category_service.js
+++ api/controllers/category_service.js
@@ -1,7 +1,7 @@
 require('../../utils/global')
 let categorySchema=require('../../db/models/Category')
 
-module.exports={getCategories}
+module.exports={getCategories,addCategory}
 
 
 /**
@@ -27,5 +27,34 @@ function getCategories(req,res,next) {
     })
 
 
+
+}
+
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {next} next 
+ */
+function addCategory(req,res,next) {
+        var category=new categorySchema({
+            title:req.body['name'],
+            language:req.body['language']
+        })
+
+        category.save(function(err,category){
+            if(err){
+                createCommonResponse(ERROR_CODE,ERROR_MESSAGE,function(callback){
+                    res.status(callback.code)
+                    res.send(callback)
+                 })
+            }else{
+                createCommonResponse(SUCCESS_CODE,SUCCESS_MESSAGE,function(callback){
+                    res.status(callback.code)
+                    res.send(callback)
+                 })
+            }
+        })
 
 }
--- utils/global.js
+++ utils/global.js
@@ -4,3 +4,11 @@ global.ERROR_MESSAGE="There is a problem"
 
 global.SUCCESS_CODE=200;
 global.SUCCESS_MESSAGE="OK";
+
+
+
+
+// Language 
+global.TR='TR'
+global.ENG='ENG'
+global.FRA='FRA'
